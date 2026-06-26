c92dd4104aa2f01a5630ab37d5f7a35b36b1499a
Melchi Arnaud <arnaudmelchi@gmail.com>
2026-06-26 08:11:29 -0400

    ajout des migrations

---
+++ database/migrations/2026_06_25_131115_create_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title',255);
+            $table->longText('content');
+            $table->string('slug',255)->unique();
+            $table->timestamp('published_at')->nullable();
+            $table->enum('status', ['draft', 'published'])->default('draft');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
