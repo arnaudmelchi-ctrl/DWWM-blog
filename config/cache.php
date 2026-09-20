5629c95080ade0a03d03bbd99a20a2318f72a4d6
Melchi Arnaud <arnaudmelchi@gmail.com>
2026-09-20 13:14:14 -0400

    Nettoyage des fichiers de configuration en supprimant les commentaires superflus dans app.php et auth.php.

--- config/cache.php
+++ config/cache.php
@@ -107,29 +107,11 @@ return [
 
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cache Key Prefix
-    |--------------------------------------------------------------------------
-    |
-    | When utilizing the APC, database, memcached, Redis, and DynamoDB cache
-    | stores, there might be other applications using the same cache. For
-    | that reason, you may prefix every cache key to avoid collisions.
-    |
-    */
+   
 
     'prefix' => env('CACHE_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-cache-'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Serializable Classes
-    |--------------------------------------------------------------------------
-    |
-    | This value determines the classes that can be unserialized from cache
-    | storage. By default, no PHP classes will be unserialized from your
-    | cache to prevent gadget chain attacks if your APP_KEY is leaked.
-    |
-    */
+   
 
     'serializable_classes' => false,
 
